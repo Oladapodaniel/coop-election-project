e79ca695b6829b2af6618343ea3bf81fd1b2e28f
Oladapo Daniel <oladapodaniel10@gmail.com>
2020-10-15 11:27:57 +0100

    Table made scrollable for small screens and hamburger made functional

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ let hamburger = document.querySelector('#hamburger');
 let navList = document.querySelector('#dashboard-left-nav');
 hamburger.addEventListener('click', function (e){
     if (navList) {
-        navList.style.marginLeft = '0px'
-    }
-    
+        navList.style.marginLeft = '0px';
+        hamburger.style.display = 'none';
+    } 
+    // else {
+    //     navList.style.marginLeft = '-800px';
+    //     hamburger.innerHTML = "<img src=\"./assets/ICON/SVG ICON/close.svg\">"
+        
+    // }
     
 })
 
 let close = document.querySelector('#close');
 close.addEventListener('click', function (e) {
     navList.style.marginLeft = '-800px';
+    hamburger.style.display = 'block';
 })
